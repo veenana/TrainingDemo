1112a3ce05eee895c8bffe66c92facf584df6c5a
Chanachai <chanachai@motiftech.com>
2017-09-07 16:52:52 +0700

    no message

--- Hello.java
+++ Hello.java
@@ -3,11 +3,16 @@ class Hello {
     public Hello() {
         System.out.println("Hello! We are:");
         this.Krerk();
+        this.Chanachai();
     }
     
     private void Krerk() {
         System.out.println("Trainer - Krerk Piromsopa, Ph.D.");
         System.out.println("Trainee - Krerk Piromsopa, Ph.D. --- ");
+    }
+    
+    private void Chanachai() {
+        System.out.println("Trainee - Chanachai");
     }
     
     public static void main(String [] argv) {
