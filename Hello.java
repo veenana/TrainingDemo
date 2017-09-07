bb1b31bf18267918f7916201723ac7c7938eb467
unknown <piyapat.p@PC-5803-0007.motif.corp>
2017-09-07 16:44:02 +0700

    Commit Piyapat Method

--- Hello.java
+++ Hello.java
@@ -3,12 +3,18 @@ class Hello {
     public Hello() {
         System.out.println("Hello! We are:");
         this.Krerk();
+		this.Piyapat();
     }
     
     private void Krerk() {
         System.out.println("Trainer - Krerk Piromsopa, Ph.D.");
         System.out.println("Trainee - Krerk Piromsopa, Ph.D. --- ");
     }
+	
+	private void Piyapat(){
+		 System.out.println("Piyapat Plydaung");
+        
+	}
     
     public static void main(String [] argv) {
         new Hello();
